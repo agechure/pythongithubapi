281fdc6377bdd1bd2965831484a7c7cb2f05ca17
abelgechure <agechure@fubo.tv>
2022-08-29 22:13:37 -0400

    Fixed a typo

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name='python_github_api',
-    version='0.0.1'
+    version='0.0.1',
     description='Simple methods to get most starred python github repositories',
     url='https://github.com/agechure/python-github-api',
     author='Abel Gechure',
     author_email='agechure@fubo.tv',
     license='MIT',
-    packages=['python_github_api']
-    install_requires=['requests','plotly']
+    packages=['python_github_api'],
+    install_requires=['requests','plotly'],
 
 )
