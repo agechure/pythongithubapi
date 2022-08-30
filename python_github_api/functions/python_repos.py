7712de839e97c4466de5f693e8ec110a4680a6e0
abelgechure <agechure@fubo.tv>
2022-08-29 21:59:39 -0400

    Createda python package with one module containing two functions to interact with the github api

---
+++ python_github_api/functions/python_repos.py
@@ -0,0 +1,35 @@
+
+import requests 
+def  repos():
+    """This function is responsible for Testing the github repository api and to print reponse data on the terminal for inspection"""
+    url = 'https://api.github.com/search/repositories?q=language:python&sort-stars'
+    headers = {'Accept': 'application/vnd.github.v3+json'}
+    r = requests.get(url, headers=headers) 
+    print(f"Status code: {r.status_code}") 
+
+    # store API response in a variable 
+    response_dict = r.json() 
+
+    # Process results 
+
+    print(response_dict.keys())
+    print(f"Total repositories: {response_dict['total_count']}")
+
+    # Explore information about the repositories 
+    repo_dicts = response_dict['items']
+    print(f"Repositories returned: {len(repo_dicts)}") 
+
+    # Summarizing the Top repositories 
+    print("\nSelected information about each repository")
+    for repo_dict in repo_dicts: 
+        print(f"\nName: {repo_dict['name']}")
+        print(f"Owner: {repo_dict['owner']['login']}")
+        print(f"Stars: {repo_dict['stargazers_count']}")
+        print(f"Repository: {repo_dict['html_url']}")
+        print(f"Created: {repo_dict['created_at']}")
+        print(f"Updated: {repo_dict['updated_at']}") 
+        print(f"Description: {repo_dict['description']}")
+
+repos()
+
+
