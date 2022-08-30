255a54fe342bdba1baf7ac193ad968be8d88d27f
abelgechure <agechure@fubo.tv>
2022-08-29 23:26:21 -0400

    renamed the package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup 
 
 setup(
-    name='python_github_api',
+    name='pythongithubapi',
     version='0.0.1',
     description='Simple methods to get most starred python github repositories',
     url='https://github.com/agechure/pythongithubapi',
